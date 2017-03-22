e026f68baa9b5b812b6d58889a5b8a04091e6a05
Vincent Botteman <vincent.botteman@gmail.com>
2017-03-23 00:54:09 +0100

    Downloading template from Spring Initializr

---
+++ src/main/java/net/persgroep/aws/cda/AwsCdaDemoApplication.java
@@ -0,0 +1,12 @@
+package net.persgroep.aws.cda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCdaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCdaDemoApplication.class, args);
+	}
+}
